a7c4e9c6ae49ffcc034af16ddb3299a24ab033bb
vikash kumar <vikashkumar24041997@gmail.com>
2025-04-10 02:58:18 +0530

    test

--- 1-basic/12-uniqueCharInStr.js
+++ 1-basic/12-uniqueCharInStr.js
@@ -1,14 +1,25 @@
-// Method-1
-function uniqueStr(str) {
-  let uniStr = "";
-  for (let char of str.toLowerCase()) {
-    if (!uniStr.includes(char)) uniStr = uniStr + char;
-  }
-  return uniStr;
-}
+// // Method-1
+// function uniqueStr(str) {
+//   let uniStr = "";
+//   for (let char of str.toLowerCase()) {
+//     if (!uniStr.includes(char)) uniStr = uniStr + char;
+//   }
+//   return uniStr;
+// }
 
-// Method-2
-function uniStr1() {
-  let uniqueString = "";
+// // Method-2
+// function uniStr1() {
+//   let uniqueString = "";
+// }
+// console.log(uniqueStr("viikash"));
+
+function uniqueString(str) {
+  let u = "";
+  for (let char of str.toLowerCase()) {
+    if (!u.includes(char)) {
+      u += char;
+    }
+  }
+  return u;
 }
-console.log(uniqueStr("viikash"));
+console.log(uniqueString("vikass"));
