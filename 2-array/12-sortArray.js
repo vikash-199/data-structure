222aad68d79b385beb056cb392d848248115eb00
vikash kumar <vikashkumar24041997@gmail.com>
2025-08-22 17:40:04 +0530

    sort arry

---
+++ 2-array/12-sortArray.js
@@ -0,0 +1,13 @@
+function sortArray(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(sortArray([1, 2, 3, 1, 4, 7, 8]));
