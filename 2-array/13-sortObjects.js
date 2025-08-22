d6cba7d34b1727c42a297247433afd2f31a1960f
vikash kumar <vikashkumar24041997@gmail.com>
2025-08-22 23:20:34 +0530

    test

---
+++ 2-array/13-sortObjects.js
@@ -0,0 +1,19 @@
+let users = [
+  { id: 3, name: "Charlie" },
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+];
+
+function bubbleSortByName(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j].name > arr[j + 1].name) {
+        // swap objects
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSortByName(users));
