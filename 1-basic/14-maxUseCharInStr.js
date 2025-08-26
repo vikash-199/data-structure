c4dee5ef6833bcda0aea1eb67af0f42608dbbfe2
vikash kumar <vikashkumar24041997@gmail.com>
2025-08-26 18:17:38 +0530

    test

--- 1-basic/14-maxUseCharInStr.js
+++ 1-basic/14-maxUseCharInStr.js
@@ -1,19 +1,20 @@
-function charCountInStr(str) {
-  const charCount = {};
+const maxUserChar = (str) => {
   let count = 0;
   let maxUseChar = "";
-  for (let char of str.toLowerCase()) {
-    if (charCount[char]) {
-      charCount[char]++;
-      if (charCount[char] > count) {
-        count = charCount[char];
-      }
+  let resObj = {};
+  for (let ele of str.toLowerCase()) {
+    if (resObj[ele]) {
+      resObj[ele]++;
     } else {
-      charCount[char] = 1;
+      resObj[ele] = 1;
     }
-    maxUseChar = char;
+
+    if (resObj[ele] > count) {
+      count = resObj[ele];
+      maxUseChar = ele;
+    }
   }
   return { maxUseChar, count };
-}
+};
 
-console.log(charCountInStr("charCountInString"));
+console.log(maxUserChar("mynameisvikashi"));
--- 1-basic/8-remDubArray.js
+++ 1-basic/8-remDubArray.js
@@ -14,14 +14,3 @@ function remduplicate1(arr) {
 }
 
 console.log(remduplicate1([1, 3, 1, 4, 6, 8, 9, 9, 5, 1, 1, 1]));
-
-const remD = (arr) => {
-  let res = [];
-  for (let ele of arr) {
-    if (!res.includes(ele)) {
-      res.push(ele);
-    }
-  }
-  return res;
-};
-console.log(remD([1, 3, 5, 7, 9, 1, 3]));
