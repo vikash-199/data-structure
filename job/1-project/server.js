2a0b711a29f579462db58b5849ed72d071cc7cc9
vikash kumar <vikashkumar24041997@gmail.com>
2025-10-25 23:13:10 +0530

    server setup

--- job/1-project/server.js
+++ job/1-project/server.js
@@ -1,1 +1,13 @@
-const objects = required("./objs.js");
+import express from "express";
+
+import books from "./objs.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(books);
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
