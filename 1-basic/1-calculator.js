a6ed2a6389616e65dec413e79dc53759e33e7c34
vikash kumar <vikashkumar24041997@gmail.com>
2025-10-25 19:38:19 +0530

    books api

--- 1-basic/1-calculator.js
+++ 1-basic/1-calculator.js
@@ -39,22 +39,3 @@ const calcaulatorVikash = (a, b, oprator) => {
 };
 
 console.log(calcaulatorVikash(10, 5, "sub"));
-
-const ca = (a, b, oprator) => {
-  let res = 0;
-  switch (oprator) {
-    case "add":
-      res = a + b;
-      break;
-    case "sub":
-      res = a - b;
-      break;
-    case "mul":
-      res = a * b;
-      break;
-    default:
-      throw new Error("Invalid errror");
-  }
-  return res;
-};
-console.log(ca(10, 5, "sub"));
