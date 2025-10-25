f67e344174b38a2d5250cde0e3d702aaa19d3819
vikash kumar <vikashkumar24041997@gmail.com>
2025-10-25 23:33:55 +0530

    pagination imp

--- job/1-project/server.js
+++ job/1-project/server.js
@@ -1,10 +1,20 @@
 import express from "express";
+import cors from "cors";
 
 import books from "./objs.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(cors()); //allows your backend server (API) to be accessed by web applications running on a different domain or port.
+app.use(express.json()); //parses incoming requests with a JSON body and makes the data available in req.body.
+
+app.get("/books", (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = 8;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const paginatedBooks = books.slice(startIndex, endIndex);
   res.send(books);
 });
 
