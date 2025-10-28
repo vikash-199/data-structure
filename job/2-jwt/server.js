2d1fbb1ffa537824b1cf0e5c6e4364ad889ce3fa
vikash kumar <vikashkumar24041997@gmail.com>
2025-10-28 21:55:52 +0530

    geting all the users

--- job/2-jwt/server.js
+++ job/2-jwt/server.js
@@ -1,29 +1,73 @@
+// import express from "express";
+// import jwt from "jsonwebtoken";
+// import bcrypt from "bcryptjs";
+// import cors from "cors";
+// import bodyParser from "body-parser";
+
+// const app = express();
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// const JWT_SECRET = "mysecretkey"; // in production, store in .env
+
+// // fake users (narmal fron DB)
+// const users = [
+//   { id: 1, username: "testuser", password: bcrypt.hashSync("123456", 8) },
+// ];
+
+// // Register new user
+// app.use("/register", (req, res) => {
+//   const { username, password } = req.body;
+//   const isUserExist = users.find((u = u.username === username));
+//   if (isUserExist)
+//     return res.status(400).json({ message: "User already exist" });
+//   const hashedPassword = bcrypt.hashSync(password, 8);
+//   const newUser = { id: users.length + 1, username, password: hashedPassword };
+//   users.push(newUser);
+
+//   res.json({ message: "User registred successfully" });
+// });
+
 import express from "express";
-import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import cors from "cors";
 import bodyParser from "body-parser";
 
 const app = express();
+
+// middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-const JWT_SECRET = "mysecretkey"; // in production, store in .env
+const JWT_SECRETKEY = "mysecretkey";
 
-// fake users (narmal fron DB)
-const users = [
-  { id: 1, username: "testuser", password: bcrypt.hashSync("123456", 8) },
+let users = [
+  { id: 1, username: "vikash@1997", password: bcrypt.hashSync("123456", 8) },
 ];
 
-// Register new user
-app.use("/register", (req, res) => {
+app.get("/users", (req, res) => {
+  res.json({
+    totleUsers: users.length,
+    users,
+  });
+});
+
+app.post("/register", (req, res) => {
   const { username, password } = req.body;
-  const isUserExist = users.find((u = u.username === username));
+
+  const isUserExist = users.find((u) => u.username === username);
+
   if (isUserExist)
     return res.status(400).json({ message: "User already exist" });
-  const hashedPassword = bcrypt.hashSync(password, 8);
-  const newUser = { id: users.length + 1, username, password: hashedPassword };
-  users.push(newUser);
 
-  res.json({ message: "User registred successfully" });
+  const hashedPassword = bcrypt.hashSync(password, 8);
+
+  const newUser = { id: users.length + 1, username, password: hashedPassword };
+
+  users.push(newUser);
+  res.json({ message: "New user created" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
