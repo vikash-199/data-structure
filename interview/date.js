ca4056b70b7ecbfe024a2fde4d29b600e65e7981
vikash kumar <vikashkumar24041997@gmail.com>
2025-11-02 23:59:12 +0530

    date test

--- interview/date.js
+++ interview/date.js
@@ -35,3 +35,61 @@ console.log(d2 > d1);
 
 const res = (d2 - d1) / (1000 * 60 * 60 * 24);
 console.log(res);
+function cal(a, b, oprator) {
+  let result;
+
+  switch (oprator) {
+    case "+":
+      result = a + b;
+      break;
+    case "-":
+      result = a - b;
+      break;
+
+    case "*":
+      result = a * b;
+      break;
+    case "/":
+      result = a / b;
+      break;
+    default:
+      throw new Error("Invalid oprator");
+  }
+  return result;
+}
+
+console.log(cal(5, 2, "*"));
+
+const calcaulatorVikash = (a, b, oprator) => {
+  let res = 0;
+  switch (oprator) {
+    case "add":
+      res = a + b;
+      break;
+    case "sub":
+      res = a - b;
+      break;
+    default:
+      throw new Error("Invalid oprator");
+  }
+  return res;
+};
+
+console.log(calcaulatorVikash(10, 5, "sub"));
+
+const newCal = (a, b, oprator) => {
+  let res = 0;
+  switch (oprator) {
+    case "add":
+      res = a + b;
+      break;
+    case "sub":
+      res = a - b;
+      break;
+    default:
+      throw new Error("Invalid Oprator");
+  }
+  return res;
+};
+
+console.log(newCal(10, 3, "add"));
