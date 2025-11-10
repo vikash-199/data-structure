2db6686d7a483dfb7d3c18425cb8ab4f10590da0
vikash kumar <vikashkumar24041997@gmail.com>
2025-11-11 04:59:04 +0530

    pattern

---
+++ 4-pattern/3-numberPy.js
@@ -0,0 +1,14 @@
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+let n = 5;
+for (let i = 1; i <= n; i++) {
+  let line = "";
+  for (let j = 1; j <= i; j++) {
+    line += j + " ";
+  }
+  console.log(line);
+}
