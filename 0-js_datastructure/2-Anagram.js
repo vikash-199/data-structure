be27daddad94b8ed3ed1a0fad999b6095e40a62d
vikash kumar <vikashkumar24041997@gmail.com>
2025-11-28 00:00:00 +0530

    test

--- 0-js_datastructure/2-Anagram.js
+++ 0-js_datastructure/2-Anagram.js
@@ -7,6 +7,22 @@ ana('anagram','nagaram') true
 ana('rat','car') false
 */
 
+function sum(str1, str2) {
+  let fc1 = {};
+  let fc2 = {};
+  if (str1.length !== str2.length) return false;
+  for (let char of str1) {
+    fc1[char] = (fc1[char] || 0) + 1;
+  }
+  for (let char of str2) {
+    fc2[char] = (fc2[char] || 0) + 1;
+  }
+  for (let key in fc1) {
+    if (!(key in fc2)) return false;
+    if (fc1[char] !== fc2[key]) return false;
+  }
+}
+
 function ana(str1, str2) {
   let fc1 = {};
   let fc2 = {};
